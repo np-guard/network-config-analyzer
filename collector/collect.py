7b733d4a8d1ad7005fb01d56cf37285316e650ad
adisos <adisos@il.ibm.com>
2024-12-09 16:32:29 +0200

    add cluster context
    Signed-off-by: adisos <adisos@il.ibm.com>

--- collector/collect.py
+++ collector/collect.py
@@ -1,12 +1,20 @@
 import subprocess
 
+
+get_ctx = 'kubectl config current-context'
+print(get_ctx)
+cmdline_process = subprocess.Popen(get_ctx.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+ctx, err = cmdline_process.communicate()
+ctx_cluster = ctx.decode("utf-8").replace('/', '-').strip()
+print(ctx_cluster)
+
 resources = ['pods', 'namespaces', 'services', 'networkpolicies', 'ingresses', 'ingressclasses', 'authorizationpolicies', 'destinationrules','gateways', 'serviceentries','sidecars','virtualservices' ]
 
 for resource in resources:
-    cmd = 'kubectl get ' + resource + ' -A -o yaml' 
+    cmd = 'kubectl get ' + resource + ' -A -o yaml'
     cmdline_list = cmd.split(' ')
     print(cmdline_list)
-    log = open(resource + '.yaml', 'a')
+    log = open(ctx_cluster + '_' + resource + '.yaml', 'a')
     cmdline_process = subprocess.Popen(cmdline_list, stdout=log, stderr=subprocess.PIPE)
-    out, err = cmdline_process.communicate()    
+    out, err = cmdline_process.communicate()
     print(err)
