f0225aa891080f72a3c035bc837a075aa1187a02
Adi Sosnovich <82078442+adisos@users.noreply.github.com>
2024-12-10 08:39:14 +0200

    Update collector/collect.py
    Co-authored-by: Ziv Nevo <79099626+zivnevo@users.noreply.github.com>

--- collector/collect.py
+++ collector/collect.py
@@ -13,7 +13,7 @@ resources = ['pods', 'namespaces', 'services', 'networkpolicies', 'ingresses', '
 for resource in resources:
     cmd = 'kubectl get ' + resource + ' -A -o yaml'
     cmdline_list = cmd.split(' ')
-    print(cmdline_list)
+    print(cmd)
     log = open(ctx_cluster + '_' + resource + '.yaml', 'a')
     cmdline_process = subprocess.Popen(cmdline_list, stdout=log, stderr=subprocess.PIPE)
     out, err = cmdline_process.communicate()
