d0d7ce5d0c0c118fb41e7e2953650ecd627e3509
Ziv Nevo <nevo@il.ibm.com>
2024-12-11 11:24:26 +0200

    Include collector in network-config-analyzer package
    Signed-off-by: Ziv Nevo <nevo@il.ibm.com>

--- collector/collect.py
+++ collector/collect.py
@@ -1,21 +1,29 @@
 import subprocess
 
 
-get_ctx = 'kubectl config current-context'
-print(get_ctx)
-cmdline_process = subprocess.Popen(get_ctx.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-ctx, err = cmdline_process.communicate()
-ctx_cluster = ctx.decode("utf-8").replace('/', '-').strip()
-print(ctx_cluster)
+def collect():
+    """
+    A small utility to collect all the network-related resources in the current cluster context
+    """
+    get_ctx = 'kubectl config current-context'
+    print(get_ctx)
+    cmdline_process = subprocess.Popen(get_ctx.split(' '), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    ctx, err = cmdline_process.communicate()
+    ctx_cluster = ctx.decode("utf-8").replace('/', '-').strip()
+    print(ctx_cluster)
 
-resources = ['pods', 'namespaces', 'services', 'networkpolicies', 'ingresses', 'ingressclasses', 'authorizationpolicies', 'destinationrules','gateways', 'serviceentries','sidecars','virtualservices' ]
+    resources = ['pods', 'namespaces', 'services', 'networkpolicies', 'ingresses', 'ingressclasses',
+                 'authorizationpolicies', 'destinationrules','gateways', 'serviceentries','sidecars','virtualservices' ]
 
-for resource in resources:
-    cmd = 'kubectl get ' + resource + ' -A -o yaml'
-    cmdline_list = cmd.split(' ')
-    print(cmd)
-    log = open(ctx_cluster + '_' + resource + '.yaml', 'a')
-    cmdline_process = subprocess.Popen(cmdline_list, stdout=log, stderr=subprocess.PIPE)
-    out, err = cmdline_process.communicate()
-    if len(err) > 0:
-        print(err)
+    for resource in resources:
+        cmd = 'kubectl get ' + resource + ' -A -o yaml'
+        cmdline_list = cmd.split(' ')
+        print(cmd)
+        log = open(ctx_cluster + '_' + resource + '.yaml', 'a')
+        cmdline_process = subprocess.Popen(cmdline_list, stdout=log, stderr=subprocess.PIPE)
+        out, err = cmdline_process.communicate()
+        if len(err) > 0:
+            print(err)
+
+if __name__ == "__main__":
+    collect()
